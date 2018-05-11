5e1b7ea7766b41979b9634d4cd93956f7322ea9c
sach915 <sach915@gmail.com>
2018-05-11 14:01:47 -0400

    verfication

--- bot.py
+++ bot.py
@@ -8,6 +8,15 @@ app = flask.Flask(__name__)
 def index():
     print("Im in here")
     print(flask.request.args)
+
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if flask.request.args.get("hub.mode") == "subscribe" and flask.request.args.get("hub.challenge"):
+        if not flask.request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return flask.request.args["hub.challenge"], 200
+
+
     return "Hello World"
 
 if __name__ == "__main__":
