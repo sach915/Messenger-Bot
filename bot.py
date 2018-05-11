3e2bc1e790e4d960a45d45e474dd9e65bcc644bb
sach915 <sach915@gmail.com>
2018-05-11 13:43:16 -0400

    verification

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
 import flask
+import requests
+import os
 
 app = flask.Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
+
+    # Credit to : https://github.com/hartleybrody/fb-messenger-bot/blob/master/app.py
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
     return "Hello World"
 
 if __name__ == "__main__":
